0df7960518cbf123d5b39f16dc5efbf58d36d73d
smithwebtek <brad@smithwebtek.com>
2017-05-09 10:09:38 -0400

    update index.html

---
+++ src/demos/01_simpleDemo/index.js
@@ -0,0 +1,8 @@
+var Cheese = React.createClass({
+  render: function(){
+    return (<h3>The linter is not working as expected.</h3>)
+
+  }
+  
+
+ReactDOM.render(<Cheese />, document.getElementById('example'));
